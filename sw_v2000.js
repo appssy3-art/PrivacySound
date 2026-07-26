7533fa3374642dd05a32bea2ca952bc1ed1ac60f
appssy3-art <appssy3-art@github.com>
2026-07-26 20:47:08 +0900

    Fix: Pointed download link to official GitHub Release assets URL for standard MIME type delivery

--- sw_v2000.js
+++ sw_v2000.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = 'soundcover-vTRUSTED_GITHUB_CDN_FINAL';
+const CACHE_NAME = 'soundcover-vRELEASE_ASSET_MIME_FINAL';
 const ASSETS = [
   './',
   './index.html',
   './styles.css',
-  './app.js?v=TRUSTED_GITHUB_CDN_FINAL',
+  './app.js?v=RELEASE_ASSET_MIME_FINAL',
   './manifest.json'
 ];
 
